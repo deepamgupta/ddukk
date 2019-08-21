326489757028b708028d0f07db36d2857679296f
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2019-08-22 01:02:27 +0530

    updated short term courses

--- short_term_courses.php
+++ short_term_courses.php
@@ -76,43 +76,22 @@
             Label the data
             */
 			.roow td:nth-of-type(1):before {
-				content: "Course Name";
+				content: "S.No.";
 			}
 
 			.roow td:nth-of-type(2):before {
-				content: "Duration";
+				content: "Title of Course";
 			}
 
 			.roow td:nth-of-type(3):before {
+				content: "Duration";
+			}
+			.roow td:nth-of-type(4):before {
 				content: "No. Of. Participants";
 			}
 
-			.roow td:nth-of-type(4):before {
-				content: "Coloboration";
-			}
-
 			.roow td:nth-of-type(5):before {
-				content: "Wars of Trek?";
-			}
-
-			.roow td:nth-of-type(6):before {
-				content: "Secret Alias";
-			}
-
-			.roow td:nth-of-type(7):before {
-				content: "Date of Birth";
-			}
-
-			.roow td:nth-of-type(8):before {
-				content: "Dream Vacation City";
-			}
-
-			.roow td:nth-of-type(9):before {
-				content: "GPA";
-			}
-
-			.roow td:nth-of-type(10):before {
-				content: "Arbitrary Data";
+				content: "Collaboration";
 			}
 		}
 	</style>
@@ -152,60 +131,156 @@
 					<thead>
 						<tr>
 							<th>S.no</th>
-							<th>Course Name</th>
+							<th>Title of Course</th>
 							<th>Duration</th>
-							<th>No. Of. Participants</th>
-							<th>Coloboration</th>
-
+							<th>No. of Participants</th>
+							<th>Collaboration</th>
 						</tr>
 					</thead>
 					<tbody>
 						<tr>
-							<td>1</td>
-							<td>Matman</td>
-							<td>Chief Sandwich Eater</td>
-							<td>James</td>
-							<td>pasmes</td>
+							<td>1.</td>
+							<td>Basic Computer Hardware Maintenance</td>
+							<td>10-23 May 2018</td>
+							<td>21</td>
+							<td>HTTP Computers, Indore</td>
 						</tr>
 						<tr>
-							<td>2</td>
-							<td>Matman</td>
-							<td>Chief Sandwich Eater</td>
-							<td>James</td>
-							<td>pasmes</td>
+							<td>2.</td>
+							<td>Awareness on Healthy Snacks & Beverages</td>
+							<td>2 June 2018</td>
+							<td>100</td>
+							<td>Nestle India Pvt. Ltd., Indore</td>
 						</tr>
 						<tr>
-							<td>3</td>
-							<td>Matman</td>
-							<td>Chief Sandwich Eater</td>
-							<td>James</td>
+							<td>3.</td>
+							<td>Network Routing & Switching</td>
+							<td>2-4 July 2018</td>
+							<td>28</td>
 							<td>pasmes</td>
 						</tr>
 
 						<tr>
-							<td>4</td>
-							<td>Matman</td>
-							<td>Chief Sandwich Eater</td>
-							<td>James</td>
-							<td>pasmes</td>
+							<td>4.</td>
+							<td>Making Handicraft for Special Children</td>
+							<td>4-5 August 2018</td>
+							<td>35</td>
+							<td>Networkers Champ, Indore</td>
 						</tr>
 
 						<tr>
-							<td>5</td>
-							<td>Matman</td>
-							<td>Chief Sandwich Eater</td>
-							<td>James</td>
-							<td>pasmes</td>
+							<td>5.</td>
+							<td>Website Design & Development</td>
+							<td>6-27 August 2018</td>
+							<td>56</td>
+							<td>Workholics Infocorp, Indore</td>
 						</tr>
 
 						<tr>
-							<td>6</td>
-							<td>Matman</td>
-							<td>Chief Sandwich Eater</td>
-							<td>James</td>
-							<td>pasmes</td>
+							<td>6.</td>
+							<td>Website Design & Development at Shri Vaishnav Institute of Management</td>
+							<td>19 Sept - 3 Oct 2018</td>
+							<td>33</td>
+							<td>Workholics Infocorp, Indore</td>
 						</tr>
-
+						<tr>
+							<td>7.</td>
+							<td>Photoshop</td>
+							<td>3-9 Oct 2018</td>
+							<td>31</td>
+							<td>New Era Consultancy, Indore</td>
+						</tr>
+						<!-- <tr>
+							<td>8.</td>
+							<td>Website Design & Development at DAVV, Indore</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>9.</td>
+							<td>Nourishing Yourself at Mata Gujari College, Indore</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>10.</td>
+							<td>Workshop on Zangle/ Doodle Art</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>11.</td>
+							<td>Entrepreneurship Development Program</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>12.</td>
+							<td>Creative Craft Creation at NEEV Foundation</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>13.</td>
+							<td>Englistia</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>14.</td>
+							<td>Personality Development Thru Drametics</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>15.</td>
+							<td>PHOTOSHOP at Shri Vaishnav Institute of Management</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>16.</td>
+							<td>TALLY</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>17.</td>
+							<td>Website Design & Development</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+<tr>
+							<td>18.</td>
+							<td>GST</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>19.</td>
+							<td>TALLY</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
+						<tr>
+							<td>20.</td>
+							<td>Environment Best Out of Waste</td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr> -->
 					</tbody>
 				</table>
 			</div>
