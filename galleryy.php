573feab5d0df0091cd3554b81802d6d81e53be73
Pratik Mehta <43946536+Pratikmehta1729@users.noreply.github.com>
2019-09-07 08:55:34 +0530

    height fixed

--- galleryy.php
+++ galleryy.php
@@ -9,7 +9,7 @@ while($file=readdir($fo))
     echo "<div class=' col-md-3 gallery-grid ' onclick='openModal();currentSlide(";
     echo $n;
     echo ")'>
-    <img src='img/gallery/$dir/$file' style='width: 100%; '  class='hover-shadow cursor'>
+    <img src='img/gallery/$dir/$file' style='width: 100%; height:214px;'  class='hover-shadow cursor'>
     <p style='font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;  ' align='center'>view</p>
     </div>";
     $n=$n+1;
